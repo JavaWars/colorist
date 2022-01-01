9fe91b120f066b3952437ffd14fff9872c2c76b4
Pavel <Lazazerovich@gmail.com>
2022-01-01 20:38:15 +0200

    Image detecting core

--- core.py
+++ core.py
@@ -1,6 +1,4 @@
-#todo: do something with logging
 import cv2
-import logging
 import numpy as np
 
 print("opening source image")
@@ -8,11 +6,11 @@ img = cv2.imread("colorist.png")
 
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-#255,242,0 nice yellow color ksta
+#detecting upper and low range
 up_range = np.array([255,255,255])
 low_range = np.array([0,1,1])
 
-#detecting is in range
+#detecting intersection with range
 mask = cv2.inRange(hsv, low_range, up_range)
 
 cv2.imshow("image", img)
